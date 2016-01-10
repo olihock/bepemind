2dee0cd46ba0bc117a4f5bdc77c413f00f059965
oliver <oliver.hock@videa-services.com>
2016-01-10 17:54:35 +0100

    Implemented Motor service tasks.

--- CubeCaching/src/test/java/org/activiti/designer/test/ProcessTestConfig.java
+++ CubeCaching/src/test/java/org/activiti/designer/test/ProcessTestConfig.java
@@ -2,6 +2,6 @@ package org.activiti.designer.test;
 
 public class ProcessTestConfig {
 
-	public static final String FILENAME = "C:\\Users\\Oliver\\Development\\workspace-ev3\\CubeCaching\\MainProcess.bpmn";
+	public static final String FILENAME = "C:\\Users\\Oliver\\Development\\github\\bepemind\\CubeCaching\\MainProcess.bpmn";
 
 }
