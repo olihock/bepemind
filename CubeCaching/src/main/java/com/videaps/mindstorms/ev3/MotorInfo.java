1a7a5d1dcf658320b1faba0f32aa2ca62356d2dd
oliver <oliver.hock@videa-services.com>
2016-01-11 23:35:51 +0100

    Added copyright and class comments.

--- CubeCaching/src/main/java/com/videaps/mindstorms/ev3/MotorInfo.java
+++ CubeCaching/src/main/java/com/videaps/mindstorms/ev3/MotorInfo.java
@@ -1,29 +1,52 @@
+/*
+ * (C) Copyright 2016 Videa Project Services GmbH (http://videa-services.com)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.videaps.mindstorms.ev3;
 
-import java.util.Map;
 
-public class MotorInfo<K, V> implements Map.Entry<K, V> {
-	private K key;
-	private V value;
+/**
+ * 
+ * @author Oliver
+ *
+ * @param <K> Motor Port
+ * @param <V> Motor Type
+ */
+public class MotorInfo<K, V> {
+	private K port;
+	private V type;
 
-	public MotorInfo(K key, V value) {
-		this.key = key;
-		this.value = value;
+	public MotorInfo(K port, V type) {
+		this.port = port;
+		this.type = type;
 	}
 
-	public K getKey() { // motor port
-		return this.key;
+	public K getPort() {
+		return this.port;
 	}
 
-	public V getValue() { // motor type
-		return this.value;
+	public V getType() {
+		return this.type;
 	}
 
-	public K setKey(K key) {
-		return this.key = key;
+	public K setPort(K port) {
+		return this.port = port;
 	}
 
-	public V setValue(V value) {
-		return this.value = value;
+	public V setType(V type) {
+		return this.type = type;
 	}
+	
 }
