8f9c6029eaf2dddedfbd40f21f27d2044ecb3107
oliver <oliver.hock@videa-services.com>
2016-01-20 23:06:36 +0100

    Implemented first steps of sensor for cube finding.

--- CubeCaching/src/test/java/com/videaps/mindstorms/ev3/SingletonEV3Test.java
+++ CubeCaching/src/test/java/com/videaps/mindstorms/ev3/SingletonEV3Test.java
@@ -1,20 +1,43 @@
+/*
+ Copyright (c) 2016 Videa Project Services GmbH
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+ and associated documentation files (the "Software"), to deal in the Software without restriction, 
+ including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ and/or sell copies of the Software,and to permit persons to whom the Software is furnished to do so, 
+ subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or substantial 
+ portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT 
+ NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+ IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 package com.videaps.mindstorms.ev3;
 
 import java.net.MalformedURLException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.List;
 
 import org.junit.Test;
 
+import lejos.hardware.port.Port;
+import lejos.hardware.sensor.EV3IRSensor;
 import lejos.remote.ev3.RMIRegulatedMotor;
+import lejos.remote.ev3.RMISampleProvider;
 import lejos.remote.ev3.RemoteEV3;
 
 
 public class SingletonEV3Test {
 
+	private final String host = "192.168.173.222";
+	
 	@Test
 	public void test() throws NotBoundException, RemoteException, MalformedURLException {
-		String host = "192.168.173.31";
 		
 		RMIRegulatedMotor motorB = SingletonEV3.intitialise(host).createRegulatedMotor("B", 'L');
 		RMIRegulatedMotor motorC = SingletonEV3.intitialise(host).createRegulatedMotor("C", 'L');
@@ -29,7 +52,7 @@ public class SingletonEV3Test {
 	@Test
 	public void testTwoMotors() throws RemoteException, MalformedURLException, NotBoundException {
 		
-		RemoteEV3 ev3 = new RemoteEV3("192.168.173.31");
+		RemoteEV3 ev3 = new RemoteEV3(host);
 		System.out.println(ev3);
 		System.out.println("host="+ev3.getHost());
 		
@@ -43,8 +66,41 @@ public class SingletonEV3Test {
 		motorB.close();
 		motorC.close();
 		
-		ev3 = new RemoteEV3("192.168.173.31");
+		ev3 = new RemoteEV3(host);
 		System.out.println(ev3);
 	}
 
+	
+	@Test
+	public void testSampleProviderDistance() throws RemoteException, MalformedURLException, NotBoundException, InterruptedException {
+		SingletonEV3 ev3 = SingletonEV3.intitialise(host);
+		RMISampleProvider sampleProvider = ev3.createSampleProvider("S1", "lejos.hardware.sensor.EV3IRSensor", "Distance");
+		
+		for(int i = 0; i < 25; i++) {
+			float[] samples = sampleProvider.fetchSample();
+			for(float sample : samples) {
+				System.out.println(sample);
+			}
+			Thread.sleep(1000);
+		}
+
+		sampleProvider.close();
+	}
+	
+	
+	@Test
+	public void testIrSensor() throws RemoteException, MalformedURLException, NotBoundException {
+		SingletonEV3 ev3 = SingletonEV3.intitialise(host);
+		
+		Port port = ev3.getPort("S1");
+		
+		EV3IRSensor sensor = new EV3IRSensor(port);
+		List<String> modes = sensor.getAvailableModes();
+		System.out.println(modes);
+		System.out.println("mode="+sensor.getCurrentMode());
+		sensor.setCurrentMode("Seek");
+		System.out.println("mode="+sensor.getCurrentMode());
+		
+		sensor.close();
+	}
 }
