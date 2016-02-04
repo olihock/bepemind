0b8be33c6b60c5c252c432b888a923f088c08b0a
oliver <oliver.hock@videa-services.com>
2016-02-05 00:03:54 +0100

    Added process variables to control wheeler.

--- CubeCaching/src/main/java/com/videaps/mindstorms/ev3/MotorDelegate.java
+++ CubeCaching/src/main/java/com/videaps/mindstorms/ev3/MotorDelegate.java
@@ -42,7 +42,6 @@ public class MotorDelegate implements JavaDelegate {
 	
 	private FixedValue motorPort;
 	private FixedValue motorFunction;
-	private FixedValue rotationDegrees;
 	private FixedValue tachoCount;
 	private FixedValue immediateReturn;
 
@@ -53,10 +52,19 @@ public class MotorDelegate implements JavaDelegate {
 		String motorPortValue = ""+motorPort.getValue(execution);
 		String motorFunctionValue = ""+motorFunction.getValue(execution);
 		boolean immediateReturnValue = Boolean.valueOf(""+immediateReturn.getValue(execution));
+		
+		// This variable has been set in the Wheeler process, as it belongs to the motors
+		// that drive the Cube Finder. For example, one turn of the wheels gets the wheeler 10 cm forward.
+		String circumferenceVariable = ""+execution.getVariable("circumference");
+		// FIXME Oliver geVariable return null, even though it is set in the dataObject of the calling process Wheeler.
 
 		if(FUNCTION_ROTATE.equals(motorFunctionValue)) {
-			int rotationCountValue = Integer.valueOf(""+rotationDegrees.getValue(execution));
-			Brick.getInstance().getRegulatedMotor(motorPortValue).rotate(rotationCountValue, immediateReturnValue);
+			// This variable has been set in the super process, as it defines the rectangle within the 
+			// cube finder searches for the cube. For example it can be 100cm x 60cm. Here, only the distance
+			// of one axis is set at a time, either x-axis or y-axis.
+			Integer distanceValue = execution.getVariable("distance", Integer.class);
+			Integer rotationDegrees = ( distanceValue / Integer.valueOf(circumferenceVariable) ) * 360;
+			Brick.getInstance().getRegulatedMotor(motorPortValue).rotate(rotationDegrees, immediateReturnValue);
 		} else if(FUNCTION_START.equals(motorFunctionValue)) {
 			Brick.getInstance().getRegulatedMotor(motorPortValue).forward();
 		} else if(FUNCTION_STOP.equals(motorFunctionValue)) {
